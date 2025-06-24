53f6612e467ee61238c50f161f94e47495c7617c
ZAHIN <abuzahinmohammadnowsin@gmail.com>
2025-06-24 15:37:15 +0600

    feat(profile): added context, provider, and reducer functionality

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,44 +1,47 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import useAxios from '../hooks/useAxios';
+import { useProfile } from '../hooks/useProfile';
+import { actions } from '../actions';
 
 export default function ProfilePage() {
-    const [user, setUser] = useState(null);
-    const [posts, setPosts] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
+    const {state, dispatch} = useProfile();
     const { api } = useAxios();
     const { auth } = useAuth();
 
     useEffect(() => {
-        setLoading(true);
+        dispatch({type: actions.profile.DATA_FETCHING});
         const fetchProfile = async () => {
             try {
                 const response = await api.get(`${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth.user.id}`);
 
-                setUser(response.data.user);
-                setPosts(response.data.posts);
+                if (response.status === 200) {
+                    dispatch({
+                        type: actions.profile.DATA_FETCHED,
+                        data: response.data,
+                    });
+                }
             } catch (error) {
                 console.log(error);
-                setError(error);
-            } finally {
-                setLoading(false);
+                dispatch({
+                    type: actions.profile.DATA_FETCH_ERROR,
+                    error: error.message || 'An error occurred while fetching profile data.',
+                });
             }
         };
         fetchProfile();
-    }, [api, auth.user.id, setPosts, setUser]);
+    }, [api, auth.user.id, dispatch]);
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error loading profile: {error.message}</div>;
+    if (state?.loading) return <div>Loading...</div>;
+    if (state?.error) return <div>Error loading profile: {state?.error.message}</div>;
 
     return (
         <div>
-            WelCome, {user?.firstName} {user?.lastName}!
+            WelCome, {state?.user?.firstName} {state?.user?.lastName}!
             <p>
-                Email: {user?.email}
+                Email: {state?.user?.email}
             </p>
-            <p>{posts?.length}</p>
+            <p>{state?.posts?.length}</p>
         </div>
     );
 }
--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,21 +1,22 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Header from "../components/sections/Header";
 import { useAuth } from "../hooks/useAuth";
+import ProfileProvider from "../providers/ProfileProvider";
 
 export default function PrivateRoutes() {
     const { auth } = useAuth();
 
     return (
         <>
-            {auth?.user ? (
-                <>
+            {auth?.authToken ? (
+                <ProfileProvider>
                     <Header />
                     <main className="mx-auto max-w-3xl py-8">
                         <div className="container">
                             <Outlet />
                         </div>
                     </main>
-                </>
+                </ProfileProvider>
             ) : (
                 <Navigate to="/login" />
             )}
